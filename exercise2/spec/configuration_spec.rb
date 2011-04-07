7c5a91eeb401dd2163169dc770c307cfbe9d5a6a
Andreas Heim <andreas@heim.no>
2011-04-07 12:22:00 +0100

    wtf

--- exercise2/spec/configuration_spec.rb
+++ exercise2/spec/configuration_spec.rb
@@ -1,13 +1,11 @@
 require File.expand_path(File.dirname(__FILE__) + '/../lib/configure')
 
 describe "configurate" do
-    
   it "it accepts a block with an object" do
     configure do |config|
       config.tail_logs = true
     end.tail_logs.should be_true
   end
-  
   it "is possible to configure app server" do
     configure do |config|
       config.app_server do |app_server|
@@ -15,7 +13,6 @@ describe "configurate" do
       end
     end.app_server.admin_password.should == "secret"
   end
-  
   it "is possible to reconfigure app server" do
     conf = configure do |c|
       c.app_server do |a|
@@ -24,13 +21,9 @@ describe "configurate" do
     end
     conf.app_server.port.should == 1336
     conf.app_server.port = 1337
-    
     conf.app_server.port.should == 1337  
-      
-    
   end
 end
-
 
 describe Configuration::AppServer do
   it { should respond_to(:port)}
@@ -41,12 +34,9 @@ describe Configuration do
   it {should respond_to(:tail_logs)}
   it {should respond_to(:max_connections)}
   it {should respond_to(:admin_password)}
-  it { should respond_to(:app_server)}
-
-  
+  it {should respond_to(:app_server)}
   describe "#app_server" do
     it { subject.app_server.should be_a(Configuration::AppServer) }
-  
     it "passes arguments given in a block" do
       mock_server = double("appserver")
       mock_server.should_receive(:foo=).with("bar")
@@ -57,5 +47,3 @@ describe Configuration do
     end
   end
 end
-
-
